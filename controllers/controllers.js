eed703f8e373cfb7c2fa1c8c957138494473f5b2
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-10-25 16:37:32 +0200

    refact: rename homepage rendering

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -3,7 +3,7 @@ const pool = require('../config/dbconfig');
 exports.getAllStudents = (req, res, next) => {
     pool.query(`SELECT * FROM etudiants`, (error, result) => {
         if (error) next(error);
-        res.render("home", { etudiants: result?.rows });
+        res.render("index", { etudiants: result?.rows });
     });
 };
 
