21c17a2a67693c4821d2e03bfbf35a8d7df1fa3e
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-10-25 15:49:09 +0200

    feat: add createStudent controller

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -6,3 +6,13 @@ exports.getAllStudents = (req, res, next) => {
         res.render("home", { etudiants: result?.rows });
     });
 };
+
+exports.createStudent = (req, res, next) => {
+    const { nom, genre, adresse } = req.body;
+    pool.query(`INSERT INTO etudiants (nom, genre, adresse) VALUES (?, ?, ?)`,
+        [nom, genre, adresse],
+        (error, result) => {
+            if (error) next(error);
+            if (result) res.redirect("/");
+        });
+};
